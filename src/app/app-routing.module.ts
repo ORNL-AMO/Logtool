576c577d04a02a9b5580198cc664996011b54e94
shubhamkokul <shubhamkokul@gmail.com>
2019-06-21 16:20:42 -0400

    My Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import {HomeComponent} from './components/home/home.component';
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {TableDataComponent} from './components/table-data/table-data.component';
-import {PlotLineGraphComponent} from './components/plot-line-graph/plot-line-graph.component';
-import {PlotScatterGraphComponent} from './components/plot-scatter-graph/plot-scatter-graph.component';
 import {DayTypeCalculationComponent} from './components/day-type-calculation/day-type-calculation.component';
 import {PlotGraphComponent} from './components/plot-graph/plot-graph.component';
 
@@ -16,14 +14,6 @@ const routes: Routes = [
         path: 'table-data',
         component: TableDataComponent,
       }]
-  },
-  {
-    path: 'line-graph',
-    component: PlotLineGraphComponent,
-  },
-  {
-    path: 'scatter-graph',
-    component: PlotScatterGraphComponent
   },
   {
     path: 'day-type-calculation',
