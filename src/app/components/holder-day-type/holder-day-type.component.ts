a813dae7fe8e23f0dd5a5e2e59401e28bb88a980
aworley16 <apworley@gmail.com>
2019-07-05 14:50:53 -0400

    minior ui updates

--- src/app/components/holder-day-type/holder-day-type.component.ts
+++ src/app/components/holder-day-type/holder-day-type.component.ts
@@ -361,8 +361,6 @@ export class HolderDayTypeComponent implements OnInit {
 
 
 // **************************************************
-
-
   clicked() {
     d3.select('#grid').selectAll('*').remove();
     const width3 = d3.select('#grid').attr('viewBox');
@@ -382,27 +380,29 @@ export class HolderDayTypeComponent implements OnInit {
       })
       .entries(this.timeSeriesDayType);
 
-
+    const x_offset = 100;
     const svg = d3.select('#grid').selectAll('g')
       .data(dayList)
       .join('g')
       .attr('transform', function (d, i) {
-        return 'translate( ' + 40 + ',' + (i * (cell_dimension + 5) + 20) + ')';
+        return 'translate( ' + x_offset + ',' + (i * (cell_dimension + 5) + 20) + ')';
       });
     const weekdays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+
+    // Mon-Sun
     const header = d3.select('#grid').append('g')
       .selectAll('g')
       .data(d => weekdays)
-      .attr('transform', 'translate( 40 , 0 )')
+      .attr('transform', 'translate(12 , 0 )')
       .join('text')
       .attr('x', function (d, i) {
-        return i * (cell_dimension + 5) + cell_dimension * (1 / 3) + 30;
+        return i * (cell_dimension + 5) + cell_dimension * (1 / 3) + x_offset-10;
       })
       .attr('y', 15)
-
       .text(function (d) {
         return d;
       });
+
     // Attach squares to week groups
     const squares = svg.append('g')
       .selectAll('g')
@@ -419,6 +419,7 @@ export class HolderDayTypeComponent implements OnInit {
       .on('click', d => this.changeColor(event.target));
     // Text
 
+    //attach dates to squares
     const dateText = svg.append('g')
       .selectAll('g')
       .data(d => d.values)
@@ -436,6 +437,7 @@ export class HolderDayTypeComponent implements OnInit {
         return d.key;
       });
 
+    // attach toggles to squares
     const checkboxes = svg.append('g')
       .selectAll('g')
       .data(d => d.values)
