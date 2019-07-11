7026402f1e080d8f158185e9c56fff32b0abae51
aworley16 <apworley@gmail.com>
2019-07-11 16:28:52 -0400

    datalist added to add bin module, table added, still has bug

--- src/app/components/holder-day-type/holder-day-type.component.ts
+++ src/app/components/holder-day-type/holder-day-type.component.ts
@@ -945,6 +945,19 @@ export class HolderDayTypeComponent implements OnInit {
     return s.color === strColor;
   }
 
+  // ------------------------------------------------------------------
+
+  realignGrid() {
+
+    console.log(this.sumArray[0].length);
+    document.getElementById('bin-panel').style.minHeight = this.sumArray === undefined ? '605px' :
+                                                                    this.sumArray[0].length <= 1 ? '605px' :
+                                                                    this.sumArray[0].length == 2  ?  '625px' : '652px';
+    console.log(document.getElementById('bin-panel').style.minHeight);
+
+  }
+
+
   plotShift(event) {
     console.log();
     if (event.target.value === 'bin') {
@@ -1113,7 +1126,8 @@ export class HolderDayTypeComponent implements OnInit {
       }
       this.sumArray.push(tempSumArray);
     }
-    console.log(this.sumArray);
+    //console.log(this.sumArray[0][0].binColor);
+    this.realignGrid();
     this.plotGraphBinAverage(0);
   }
 }
