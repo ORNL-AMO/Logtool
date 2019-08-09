4bc9d9969afac71646ac7c767ad12b3968a03aaa
aworley16 <apworley@gmail.com>
2019-08-09 13:57:57 -0400

    fixed mulit-tab add issue

--- src/app/components/import-data/import-data.component.ts
+++ src/app/components/import-data/import-data.component.ts
@@ -53,6 +53,7 @@ export class ImportDataComponent implements OnInit {
   private manualSample: any[];
 
   public tableItem: Subject<any>;
+  public id: Subject<number>;
 
   testModRef: BsModalRef;
   type;
@@ -73,6 +74,7 @@ export class ImportDataComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.id = new Subject();
     this.stage = 1;
     this.headerFind = 'auto';
   }
@@ -155,7 +157,7 @@ export class ImportDataComponent implements OnInit {
       check1 = checkHeader.length;
 
       if (check3 === check1 && check3 === check2) {
-        console.log(headerIndex);
+        //console.log(headerIndex);
         headerIndex = headerIndex - 2;
         checkHeader = Object.values(this.dataArrayColumns[headerIndex]);
         break;
@@ -242,12 +244,12 @@ export class ImportDataComponent implements OnInit {
     const timeCheck = /[0-9][0-9]:[0-9][0-9]/;
     const dateCheck = /[0-9]-[0-9]|\/\/|\/[0-9]/;
 
-    console.log(new Date('6-30-1991 5:00'));
+   // console.log(new Date('6-30-1991 5:00'));
 
     const timelist = [];
 
     const index = headerIndex + 1;
-    console.log(this.dataArrayColumns[index - 1]);
+   // console.log(this.dataArrayColumns[index - 1]);
     for (let i = 0; i < this.header.length; i++) {
       // column is timeSeries if not a number and parsable as date , or matches regex above
       if ((isNaN(parseInt(this.dataArrayColumns[index][i], 10)) && !isNaN(Date.parse(this.dataArrayColumns[index][i]))) ||
@@ -352,6 +354,7 @@ export class ImportDataComponent implements OnInit {
 
   submitCheckBox() {
 
+
     if ( !this.headersfilled()) {
       alert('Not all selected columns are named, please name all selected columns');
       return;
@@ -380,6 +383,7 @@ export class ImportDataComponent implements OnInit {
     }
     this.dataArrayColumns = holder;
     const id = this.data.getRandomInt(9999999);
+
     const dataList: CSVFileInput = {
       id: id,
       name: this.alias,
@@ -396,11 +400,15 @@ export class ImportDataComponent implements OnInit {
       fileType: this.fileType,
       dateUpload: 'DateUpload'
     };
-    this.indexFileStore.insertIntoCSVStore(dataList);
+    this.indexFileStore.insertIntoCSVStore(dataList).then( () => {
+
+      this.id.next(id);
+    });
     setTimeout(() => {
       this.bsModalRef.hide();
       console.log('Send Data');
     }, 2000);
+
   }
 
   columnNameChange(event) {
